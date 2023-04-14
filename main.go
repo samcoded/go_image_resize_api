04b1de6094d7a1a757c6ccf14d8a29c388cd9799
Samuel Anozie <samuelanozie10@yahoo.com>
2023-04-14 19:03:29 +0100

    Route set up

---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"log"
+
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	"github.com/samcoded/go_image_resize_api/controllers"
+)
+
+func init() {
+
+	// load .env file
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+}
+
+func main() {
+	r := gin.Default()
+	r.GET("/images", controllers.ImagesResize)
+	r.Run() // listen and serve
+}
