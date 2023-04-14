169d6bf16997721207db4e9dc84be81467c64c55
Samuel Anozie <samuelanozie10@yahoo.com>
2023-04-14 19:04:15 +0100

    feat: controller for image reuest

---
+++ controllers/imageController.go
@@ -0,0 +1,31 @@
+package controllers
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/samcoded/go_image_resize_api/utils"
+)
+
+func ImagesResize(c *gin.Context) {
+	url := c.Query("url")
+	width := c.Query("w")
+	height := c.Query("h")
+
+	resiveImg, err := utils.Resize(url, width, height)
+	if err != nil {
+		c.JSON(400, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	// serve the image to the client
+	c.Header("Content-Type", "image/jpeg")
+	c.File(resiveImg)
+
+	// c.JSON(200, gin.H{
+	// 	"url":    url,
+	// 	"width":  width,
+	// 	"height": height,
+	// 	"img":    "dd",
+	// })
+}
